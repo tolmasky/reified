ec0426c25a7fa93895b7e1da1e2f77b87e4a9d8b
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2023-01-24 09:53:09 -0500

    Add more intrinsics.
    Reviewed by @tolmasky.

--- intrinsics/intrinsics.js
+++ intrinsics/intrinsics.js
@@ -14,9 +14,14 @@ const Call = (I.Function.prototype.call).bind(I.Function.prototype.call);
 I.Array = global.Array;
 I["Array.from"] = I.Array.from;
 I["Array.isArray"] = I.Array.isArray;
+I["Array.prototype.concat"] = I.Array.prototype.concat;
+I["Array.prototype.filter"] = I.Array.prototype.filter;
 I["Array.prototype.forEach"] = I.Array.prototype.forEach;
 I["Array.prototype.map"] = I.Array.prototype.map;
+I["Array.prototype.push"] = I.Array.prototype.push;
 I["Array.prototype.reduce"] = I.Array.prototype.reduce;
+I["Array.prototype.slice"] = I.Array.prototype.slice;
+I["Array.prototype.sort"] = I.Array.prototype.sort;
 
 I.Error = global.Error;
 I.AggregateError = global.AggregateError;
@@ -26,6 +31,8 @@ I.ReferenceError = global.ReferenceError;
 I.SyntaxError = global.SyntaxError;
 I.TypeError = global.TypeError;
 I.URIError = global.URIError;
+
+I.isNaN = global.isNaN;
 
 I.Object = global.Object;
 I["Object.assign"] = I.Object.assign || function assign(object)
@@ -63,11 +70,29 @@ I["Object.fromEntries"] = I.Object.fromEntries || function fromEntries(entries)
         (object, [key, value]) => (object[key] = value, object),
         { });
 };
+I["Object.hasOwn"] = I.Object.hasOwn || function hasOwn(object, key)
+{
+    return Call(I["Object.hasOwnProperty"], object, key);
+};
 I["Object.hasOwnProperty"] = I.Object.hasOwnProperty;
 I["Object.keys"] = I.Object.keys;
 
 I["Object.getOwnPropertyDescriptor"] = I.Object.getOwnPropertyDescriptor;
 I["Object.getPrototypeOf"] = I.Object.getPrototypeOf;
+
+I["RegExp"] = I.RegExp;
+I["RegExp.prototype.exec"] = I.RegExp.prototype.exec;
+
+I["String"] = global.String;
+I["String.prototype.charCodeAt"] = I.String.prototype.charCodeAt;
+I["String.prototype.match"] = I.String.prototype.match;
+I["String.prototype.indexOf"] = I.String.prototype.indexOf;
+I["String.prototype.slice"] = I.String.prototype.slice;
+I["String.prototype.split"] = I.String.prototype.split;
+I["String.prototype.substr"] = I.String.prototype.substr;
+I["String.prototype.substring"] = I.String.prototype.substring;
+
+I["undefined"] = void(0);
 
 I["JSON"] = JSON;
 I["JSON.stringify"] = JSON.stringify;
