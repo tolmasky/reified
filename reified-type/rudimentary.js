e033f3acb84c5c7f8f6af6b3d2aede0bdcb2613d
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2023-05-11 16:19:04 -0400

    More work on Rudimentary.
    Reviewed by @tolmasky.

--- reified-type/rudimentary.js
+++ reified-type/rudimentary.js
@@ -1,21 +1,41 @@
 const given = f => f();
 
-const { I } = require("@reified/ecma-262");
-const { ƒnamed } = require("@reified/core/function-objects");
+const { I, HasOwnProperty } = require("@reified/ecma-262");
+const { ƒextending } = require("@reified/core/function-objects");
 
 const Declaration = require("./declaration");
+const typecheck = require("./type-check");
 
-const DataProperty = (value, enumerable = false) => ({ value, enumerable });
+const Symbols = { extending: Symbol("extending") };
 
+
+const toDataProperty = ({ binding, type }, location, source) =>
+    !HasOwnProperty(source, binding) ?
+        fail.type(
+            `No value was provided for required field "${binding}" in ` +
+            `${reference}.`) :
+        ({
+            value: typecheck(
+                type,
+                source[binding],
+                `value for field "${binding}" in ${location}`),
+            enumerable: true
+        });
 
 module.exports = Declaration `Rudimentary` (({ binding, body }) => given((
-    fields = I `Object.entries` (body)) =>
-    ƒnamed(binding, function R(properties)
+    { [Symbols.extending]: extending = I `Object`, ...rest } = body,
+    fields = I `Object.entries` (rest)
+        [I `::Array.prototype.map`]
+            (([binding, type]) => ({ type, binding }))) =>
+    ƒextending(extending, binding, function R(properties)
     {
         return I `Object.create` (R.prototype,
             I `Object.fromEntries` (fields
-                [I `::Array.prototype.map`]
-                    (([key]) => [key, DataProperty(body[key], true)])));
+                [I `::Array.prototype.map`] (field =>
+                [
+                    field.binding,
+                    toDataProperty(field, binding, properties)
+                ])));
     })));
     /*
 
