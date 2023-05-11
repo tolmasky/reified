af2493d02c917d8b4b21f9957d362ad73d677b14
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2023-05-11 11:30:37 -0400

    Minor fix of giving type function a name.
    Reviewed by @tolmasky.

--- reified-type/reified-type.js
+++ reified-type/reified-type.js
@@ -49,7 +49,7 @@ const TypeDefinition = BasicFactory `TypeDefinition` (declaration => given((
 ({
     binding,
     constructors,
-    implementation: function T(...args)
+    implementation: ƒnamed(binding, function T(...args)
     {
         if (IsTaggedCall(args))
             return annotated(ToResolvedString(args), T);
@@ -61,7 +61,7 @@ const TypeDefinition = BasicFactory `TypeDefinition` (declaration => given((
                 I `Object.keys` (constructors) [ `::Array.prototype.join`] (`\n`));
 
         return constructors[binding](...args);
-    }
+    })
 })));
 
 const type = Declaration `type` (({ binding, body }) => given((
@@ -105,114 +105,3 @@ type.PropertyKey = primitive `PropertyKey` (IsPropertyKey);
 
 
 module.exports = type;
-
-/*
-
-const BasicFactory = Declaration `BasicFactory` ({ binding, })
-
-function TypeDefinition()
-{
-}
-
-const { I, IsArray } = require("@reified/ecma-262");
-
-const Declaration = require("@reified/core/declaration");
-const Definition = require("@reified/core/definition");
-const fail = require("@reified/core/fail");
-const { ƒnamed, IsTaggedCall } = require("@reified/core/function-objects");
-
-const
-{
-    ValueConstructorDeclaration,
-    ValueConstructorDefinition,
-    ValueConstructorSymbols,
-    GetValueConstructorOf,
-    GetValueConstructorDefinitionOf,
-    fromParsedConstructorName
-} = require("./value-constructor");
-
-const { ø, α } = require("@reified/object");
-
-
-const [TypeDefinition, GetTypeDefinitionOf] = Definition `TypeDefinition`
-    ((implementation, properties) =>
-    ({
-        implementation,
-        ...properties
-    }));
-// can't rely on .length...
-const type = Declaration `type` (declaration => given((
-    binding = declaration.name,
-    [name] = fromParsedConstructorName(binding),
-    { tail: [cases, ...rest] } = declaration,
-    T = ƒnamed(name, function (...argumentsList)
-    {
-        const constructor = definition.constructors[name];
-
-        if (!constructor)
-            return fail.type (
-                `${name} cannot be directly instantiated, ` +
-                `use one if it's constructors.`);
-
-        return constructor(...argumentsList);
-    },
-    {
-        get length() { return definition.constructors[name].length; },
-        toString()
-        {
-            return definition.constructors[name].toString()
-        }
-    }),
-    ValueConstructorDefinitions = !IsArray(cases) ?
-        [ValueConstructorDefinition(T, { binding, tail: [cases] })] :
-        I `Array.from` (
-            cases,
-            declaration => ValueConstructorDefinition(T, declaration)),
-    definition = TypeDefinition(T,
-    {
-        constructors: ø.fromEntries(I `Array.from` (
-            ValueConstructorDefinitions,
-            definition => [definition.name, definition.implementation]))
-    }),
-    shorthands = ø.fromEntries(I `Array.from` (
-        ValueConstructorDefinitions,
-        definition =>
-        [
-            definition.name,
-            definition.isConstant ?
-                definition.instance :
-                definition.implementation
-        ])),
-    { prototype, ...properties } = ø(...rest)) => I `Object.assign` (T,
-    {
-        prototype: α(T.prototype, prototype),
-
-        ...shorthands,
-        ...properties
-    })));
-
-const caseof = (...argumentsList) => IsTaggedCall(argumentsList) ?
-    ValueConstructorDeclaration(...argumentsList) :
-    given((
-        [value, cases] = argumentsList,
-        { default: fallback } = cases,
-        C = GetValueConstructorOf(value),
-        { name, symbol } = GetValueConstructorDefinitionOf(C),
-        match = cases[symbol] ||
-                cases[name] ||
-                cases.default) => !match ?
-            fail.type (`No match for ${name} found in caseof statement`) :
-            match(value));
-
-module.exports = I `Object.assign` (type,
-{
-    type,
-
-    of: value => I `Object.getPrototypeOf` (value) .constructor,
-
-    caseof,
-
-    ...ValueConstructorSymbols
-});
-
-*/
