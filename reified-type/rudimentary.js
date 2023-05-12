fd4f102ddcfc0949efbb6477e2bf5577c8cec49f
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2023-05-11 20:26:58 -0400

    Support computed fields in Rudimentary types.
    Reviewd by @tolmasky.

--- reified-type/rudimentary.js
+++ reified-type/rudimentary.js
@@ -1,6 +1,13 @@
 const given = f => f();
 
-const { I, HasOwnProperty, IsString } = require("@reified/ecma-262");
+const
+{
+    I,
+    HasOwnProperty,
+    IsArray,
+    IsString,
+    IsFunctionObject
+} = require("@reified/ecma-262");
 const { ƒextending } = require("@reified/core/function-objects");
 
 const Declaration = require("./declaration");
@@ -9,34 +16,40 @@ const typecheck = require("./type-check");
 const Symbols = { extending: Symbol("extending") };
 
 
-const toDataProperty = ({ binding, type }, location, source) =>
-    !HasOwnProperty(source, binding) ?
+const toDataProperty = ({ binding, type, compute }, location, source) =>
+    !compute && !HasOwnProperty(source, binding) ?
         fail.type(
             `No value was provided for required field "${binding}" in ` +
             `${reference}.`) :
         ({
             value: typecheck(
                 type,
-                source[binding],
+                compute ? compute(source) : source[binding],
                 `value for field "${binding}" in ${location}`),
             enumerable: true
         });
 
-const Rudimentary = Declaration `Rudimentary` (({ binding, body }) => given((
-    { [Symbols.extending]: extending = I `Object`, ...rest } = body,
-    fields = I `Object.entries` (rest)
-        [I `::Array.prototype.map`]
-            (([binding, type]) => ({ type, binding }))) =>
-    ƒextending(extending, binding, function R(properties)
-    {
-        return I `Object.create` (R.prototype,
-            I `Object.fromEntries` (fields
-                [I `::Array.prototype.map`] (field =>
-                [
-                    field.binding,
-                    toDataProperty(field, binding, properties)
-                ])));
-    })));
+const Rudimentary = Declaration `Rudimentary` (({ binding, body }) =>
+    /*IsFunctionObject(body) ?
+        :
+*/
+    given((
+        { [Symbols.extending]: extending = I `Object`, ...rest } = body,
+        fields = I `Object.entries` (rest)
+            [I `::Array.prototype.map`] (([binding, body]) =>
+                IsArray(body) ?
+                    ({ binding, type: body[0], compute: body[1] }) :
+                    ({ binding, type: body }))) =>
+        ƒextending(extending, binding, function R(properties)
+        {
+            return I `Object.create` (R.prototype,
+                I `Object.fromEntries` (fields
+                    [I `::Array.prototype.map`] (field =>
+                    [
+                        field.binding,
+                        toDataProperty(field, binding, properties)
+                    ])));
+        })));
 
 module.exports = I `Object.assign` (Rudimentary, { Rudimentary }, Symbols);
 
