82f20dad2bb4f3bacbf33fd723087f2054745d4f
Francisco Tolmasky <tolmasky@gmail.com>
2024-02-01 13:17:22 -0500

    Correct comments in Set file.
    Reviewed by @tolmasky.

--- ecma-262/set-objects.mjs
+++ ecma-262/set-objects.mjs
@@ -1,7 +1,12 @@
-// 24.2.1 The Set Constructor
-// https://tc39.es/ecma262/#sec-set-constructor
+// 24.2 Set Objects
+// https://tc39.es/ecma262/#sec-set-objects
+
+
+// 24.2.1.1 Set ( [ iterable ] )
+// https://tc39.es/ecma262/#sec-set-iterable
 
 export const Set = globalThis.Set;
+
 
 // 24.2.3.7 Set.prototype.has
 // https://tc39.es/ecma262/#sec-set.prototype.has
